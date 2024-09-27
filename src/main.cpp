2a6df945355440ca983de3bd5914cd8dd9d83ed6
torph <73564623+Torphedo@users.noreply.github.com>
2024-09-26 23:09:22 -0400

    Minor refactoring, set clear color

--- src/main.cpp
+++ src/main.cpp
@@ -4,52 +4,55 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
 /**
  * Starts GLFW and creates a new window
  * @return Window pointer
  */
-GLFWwindow* startUp() {
+GLFWwindow* startUp(int width, int height) {
     if(!glfwInit()) {
-        printf("Failed to initialize GLFW3");
+        printf("Failed to initialize GLFW3\n");
         return NULL;
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3.3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3.3);
+    // We want an OpenGL 3.3 Core context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //Create a window
-    GLFWwindow* window = glfwCreateWindow(640,480, "Walrus", NULL, NULL);
-    //If window fails
-    if(window == NULL){
-        printf("Window or OpenGL context creation failed");
+
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(width, height, "Walrus", NULL, NULL);
+    if(window == NULL) {
+        printf("Window or OpenGL context creation failed\n");
         return NULL;
     }
-    //Make context current
+
+    // Load OpenGL context
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    glViewport(0, 0, width, height);
+    glfwSwapInterval(1); // VSync
+
     return window;
 }
+
 /**
  * Main Function
  */
 int main(){
-    //Starting GLFW
-    GLFWwindow* window = startUp();
-    int width = 1000;
-    int height = 1000;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-    if(window == nullptr){
-        printf("Window is NULL");
+    // Starting GLFW
+    const int width = 1000;
+    const int height = 1000;
+    GLFWwindow* window = startUp(width, height);
+    if(window == nullptr) {
+        printf("Setup failed\n");
         return 1;
     }
-    while(!glfwWindowShouldClose(window))
-    {
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
-        glfwSwapInterval(1);
         glfwPollEvents();
-
-
     }
     glfwDestroyWindow(window);
     glfwTerminate();
